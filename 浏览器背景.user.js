84140fd3fe1928789d9a74088689b1d41f965dc2
mingming <mingming28839@gmail.com>
2026-08-13 15:17:12 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- 浏览器背景.user.js
+++ 浏览器背景.user.js
@@ -2,7 +2,7 @@
 // @name         浏览器背景 v3.0
 // @namespace    https://viayoo.com/
 // @version      3.0
-// @description  浏览器背景 (修复部分传统网站骨架容器不透明问题)
+// @description  浏览器背景
 // @author       ChatGPT & MiMo
 // @match        *://*/*
 // @run-at       document-start
